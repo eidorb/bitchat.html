674445d3aa14604747e29a7a0ebebe797890de5f
eidorb <brodieface@gmail.com>
2025-07-20 16:54:13 +1000

    Show help in startup message

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -2,31 +2,16 @@ const color= "\x1b[1;35m";
 const underline= "\x1b[94;4m";
 const normal= "\x1b[0m";
 export const introMessage = [
+	"",
 	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
 	"|                                                                             |",
-	"| WebVM is a virtual Linux environment running in the browser via WebAssembly |",
+	"| Welcome to bitchat.html. Follow these steps to get started:                 |",
 	"|                                                                             |",
-	"| WebVM is powered by the CheerpX virtualization engine, which enables safe,  |",
-	"| sandboxed client-side execution of x86 binaries, fully client-side          |",
-	"|                                                                             |",
-	"| CheerpX includes an x86-to-WebAssembly JIT compiler, a virtual block-based  |",
-	"| file system, and a Linux syscall emulator                                   |",
-	"|                                                                             |",
-	"| Try out the new Alpine / Xorg / i3 WebVM: " + underline + "https://webvm.io/alpine.html" + normal + "      |",
-	"|                                                                             |",
-	"| [News] Integrating Claude Computer Use in WebVM!                            |",
-	"|                                                                             |",
-	"| " + underline + "https://labs.leaningtech.com/blog/webvm-claude" + normal + "                              |",
+	"|    1. Connect to Tailscale (sidebar)                                        |",
+	"|    2. TODO: Connect to USB/IP server                                        |",
+	"|    3. Run bitchat: python3 bitchat.py                                       |",
 	"|                                                                             |",
 	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
-	"",
-	"   Welcome to WebVM. If unsure, try these examples:",
-	"",
-	"     python3 examples/python3/fibonacci.py ",
-	"     gcc -o helloworld examples/c/helloworld.c && ./helloworld",
-	"     objdump -d ./helloworld | less -M",
-	"     vim examples/c/helloworld.c",
-	"     curl --max-time 15 parrot.live  # requires networking",
 	""
 ];
 export const errorMessage = [
